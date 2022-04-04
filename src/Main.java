27c63a4e768b64cf34fd947bde43ced2826a5251
marcelcg8011 <86576646+marcelcg8011@users.noreply.github.com>
2022-04-04 12:51:19 +1200

    wrote up Student class and implemented two objects in main sout the data fields

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
-public class Main {
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Student Poggers = new Student();
+        Student Champ = new Student();
+
+        Poggers.setName("Poggers");
+        Poggers.setAge(20);
+        Champ.setName("Pepe");
+        Champ.setAge(25);
+
+        System.out.println("Name is " + Poggers.getName());
+        System.out.println("Age is " + Poggers.getAge());
+        System.out.println("Name is " + Champ.getName());
+        System.out.println("Age is " + Champ.getAge());
+
+    }
 }
